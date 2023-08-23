c753eb3006e38145ac126a6dae7a740524656a3e
Aborgono <albertoiborgonovo@gmail.com>
2023-08-23 14:46:30 -0400

    second commit

---
+++ OneDrive/Desktop/reel-talk/src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDu10pVp0b1IB4ummvQwg5ynvpvOyFE0SU",
+  authDomain: "reel-18f61.firebaseapp.com",
+  projectId: "reel-18f61",
+  storageBucket: "reel-18f61.appspot.com",
+  messagingSenderId: "1090894405351",
+  appId: "1:1090894405351:web:590ae799cdb0671165cc42",
+  measurementId: "G-NJB39X6RSV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
