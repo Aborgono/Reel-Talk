4b75d81f69dcd38757e886763232a3c5317bb226
Aborgono <albertoiborgonovo@gmail.com>
2023-08-29 20:39:25 -0400

    minor adjustments to axios

--- OneDrive/Desktop/reel-talk/src/components/movieListApi.js
+++ OneDrive/Desktop/reel-talk/src/components/movieListApi.js
@@ -6,13 +6,32 @@ function MovieAPI() {
 
     const [listMovie, setListMovie] = useState([]);
 
-    useEffect(() => {
-      axios.get('https://api.themoviedb.org/3/movie/popular?api_key=9a18ce06bba8a0e8dba96a97dfd2a61e').then((response) => {
-        setListMovie(response.data.results)
-      });
-    }, []);
+    // useEffect(() => {
+    //   axios.get('https://api.themoviedb.org/3/movie/popular?api_key=9a18ce06bba8a0e8dba96a97dfd2a61e').then((response) => {
+    //     setListMovie(response.data.results)
+    //   });
+    // }, []);
 
-    console.log("this is my movie list", listMovie);
+    // console.log("this is my movie list", listMovie);
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/movie/popular',
+  params: {page: '1'},
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTE4Y2UwNmJiYThhMGU4ZGJhOTZhOTdkZmQyYTYxZSIsInN1YiI6IjY0ZWU1MjMzMTgwZGVhMDBhY2U4NzFjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gMas9RcyRtU6YZWLHAYAGjWDyF2sFLh2AvBbqHmsrCQ'
+  }
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log("THIS IS MY RESPONSE", response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
   return (
     <>
